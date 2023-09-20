2986b104e85fb68a5815a624511f51b0977c2278
Gokhan Okur <gokhanokur241@gmail.com>
2023-09-20 14:28:30 +0300

    Add project files.

---
+++ EncapsulaionProperty/EncapsulaionProperty/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulation;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*****Student 1******");
+        Student student1 = new Student("Gökhan", "Okur", 5, "IT");
+        student1.ClassNoUp();
+        student1.StudentsInfo();
+
+        Console.WriteLine("*****Student 2******");
+        Student student2 = new Student("İlkay", "Seki", 3, "IT");
+        student2.ClassNoDown();
+        student2.StudentsInfo();
+        student2.ClassNoDown();
+        student2.StudentsInfo();
+        student2.ClassNoDown();
+        student2.StudentsInfo();
+
+    }
+
+    class Student
+    {
+        private string name;
+        private string surname;
+        private int classNo;
+        private string departman;
+
+        public string Name { get => name; set => name = value; }
+
+        public string Surname { get => surname; set => surname = value; }
+
+        public int Class
+        {
+            get => classNo;
+            set
+            {
+                if(value < 1)
+                {
+                    Console.WriteLine("at least one");
+                    classNo = 1;
+                }
+                else  
+                    classNo = value;
+            }
+        }
+
+        public string Departman { get => departman; set => departman = value; }
+
+        public Student(string name, string surname, int classNo, string departman)
+        {
+            Name = name;
+            Surname = surname;
+            Class = classNo;
+            Departman = departman;
+        }
+
+        public Student() { }
+
+        public void StudentsInfo()
+        {
+            Console.WriteLine("***Student Info***");
+            Console.WriteLine("Student Name: {0}", this.Name);
+            Console.WriteLine("Student Surname: {0}", this.Surname);
+            Console.WriteLine("Student Class No: {0}", this.Class);
+            Console.WriteLine("Student Departman: {0}", this.Departman);
+            Console.ReadKey();
+        }
+
+        public void ClassNoUp()
+        {
+            this.Class = this.Class+1;
+        }
+
+        public void ClassNoDown()
+        {
+            this.Class = this.Class-1;
+        }
+    }
+}
+
---
+++ InstanceFieldProperty/InstanceFieldProperty/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InstanceFieldsProperty;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*****Student 1******");
+        Student student1 = new Student("Gökhan", "Okur", 241, "IT");
+        student1.StudentInfo();
+
+        Console.WriteLine("*****Student 2******");
+        Student student2 = new Student("İlkay", "Seki", 240, "IT");
+        student2.StudentInfo();
+
+    }
+
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public int No;
+        public string Departman;
+
+        public Student(string name, string surname, int no, string departman) // Constructor 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.No = no;
+            this.Departman = departman;
+        }
+
+
+        public void StudentInfo()
+        {
+            Console.WriteLine("Student Name: {0}", Name);
+            Console.WriteLine("Student Surname: {0}", Surname);
+            Console.WriteLine("Student No: {0}", No);
+            Console.WriteLine("Student Departman: {0}", Departman);
+            Console.ReadKey();
+        }
+    }
+}
