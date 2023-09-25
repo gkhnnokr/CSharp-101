c7812cd3d3ff8067f148af43160c2d3674e5195c
Gokhan Okur <gokhanokur241@gmail.com>
2023-09-25 17:45:00 +0300

    enum

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Enum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Days.Sunday); //Sunday
+        Console.WriteLine((int)Days.Tuesday); //1
+        Console.ReadKey();
+    }
+}
+
+enum Days
+{
+    Monday, 
+    Tuesday, 
+    Wednesday, 
+    Thursday, 
+    Friday,
+    Saturday,
+    Sunday
+}
+
